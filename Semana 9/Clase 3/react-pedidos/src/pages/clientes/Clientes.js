314cbcf7b3b9df9a0504b0775eea1d8b44281cf4
gonzalocarhuachin <lgonzalocarhuachin@gmail.com>
2020-06-06 21:53:08 -0500

    REACT consumo de clientes

--- Semana 9/Clase 3/react-pedidos/src/pages/clientes/Clientes.js
+++ Semana 9/Clase 3/react-pedidos/src/pages/clientes/Clientes.js
@@ -1,10 +1,52 @@
-import React from 'react';
+import React, { Fragment, useState,useEffect } from 'react';
+import ClienteForm from './componentes/ClienteForm';
+import Cargando from '../../components/Cargando';
+import ClientesTabla from './componentes/ClientesTabla';
 
 const Clientes = () => {
+
+    const endpoint = "https://5ec8643e155c130016a909bf.mockapi.io/cliente";
+    const [cargando, setCargando] = useState(true);
+    const [clientes, setClientes] = useState([]);
+    const [objCliente, setObjCliente] = useState(null);
+
+    const getClientes = () =>
+    {
+        if(!cargando)
+        {
+            setCargando(true);
+        }
+
+        fetch(endpoint).then((response) =>
+        {
+            response.json().then((data) =>
+            {
+                setCargando(false);
+                setClientes(data);
+            })
+        })
+    }
+
+    useEffect(() =>
+    {
+        getClientes();
+    }, [])
+
     return (
-        <div>
-            
-        </div>
+        <Fragment>
+            <div className="row justify-content-center">
+                <div className="col-md-8">
+                    <ClienteForm getClientes={getClientes} objCliente={objCliente} setObjCliente={setObjCliente}/>
+                </div>
+            </div>
+            <div className="row justify-content-center">
+                <div className="col-md-8">
+                    {
+                        cargando === true ? <Cargando tipo="info" texto="Cargando clientes"/> : <ClientesTabla clientes={clientes} setObjCliente={setObjCliente}/>
+                    }
+                </div>
+            </div>
+        </Fragment>
     );
 }
 
---
+++ Semana 9/Clase 3/react-pedidos/src/pages/clientes/componentes/ClienteFila.js
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from 'react';
+import Switch from 'react-switch';
+import { URL_BACKEND } from '../../../variables/variables';
+
+const ClienteFila = ({cliente, numero, setObjCliente}) => {
+
+    const [checked, setChecked] = useState(false);
+
+    const putCliente = () =>
+    {
+        let nuevoCliente = {...cliente}
+        nuevoCliente.cli_est = !checked + ""; //Convertir en string
+        const endpoint = `${URL_BACKEND}/cliente/${cliente.id}`;
+        fetch(endpoint, 
+            {
+                method: 'PUT',
+                headers: {"Content-type": "application/json"},
+                body: JSON.stringify(nuevoCliente)
+            }).then((response) =>
+            {
+                response.json().then((data) =>
+                {
+                    setChecked(!checked);
+                })
+            })
+    }
+
+    useEffect(() =>
+    {
+        if(cliente.cli_est === "true")
+        {
+            setChecked(true);
+        }
+    }, []);
+
+    return (
+        <tr>
+            <td>{numero}</td>
+            <td>{cliente.id}</td>
+            <td>{cliente.cli_nom}</td>
+            <td>{cliente.cli_ape}</td>
+            <td>
+                {
+                    cliente.cli_est === "true" ?
+                    <span className="badge badge-success">Activo</span> :
+                    <span className="badge badge-danger">Inactivo</span>
+                }
+                <Switch checked={checked} onChange={putCliente} />
+            </td>
+            <td>{cliente.cli_dni}</td>
+            <td><button className="btn btn-info" onClick={() => 
+                {
+                    setObjCliente({cliente});
+                }}>Editar</button></td>
+        </tr>
+    );
+}
+
+export default ClienteFila;
